92682a7c14129e99302ddf91584b1d7930fade3e
Michael Kane <kaneplusplus@gmail.com>
2015-02-16 16:52:56 -0500

    Initial commit.

---
+++ tests/testthat/ioglm-data-frame.r
@@ -0,0 +1,21 @@
+library(ioregression)
+library(testthat)
+
+data = read.csv("http://www.ats.ucla.edu/stat/data/binary.csv")
+form = admit ~ gre + gpa + rank
+glmfit = glm(form, data=data, family = binomial())
+
+iofit = ioglm(form, data = data, family = binomial())
+
+expect_equal(iofit$coefficients, glmfit$coefficients)
+expect_equal(iofit$rank, glmfit$rank)
+expect_equal(iofit$family, glmfit$family)
+expect_equal(iofit$deviance, glmfit$deviance)
+expect_equal(iofit$aic, glmfit$aic)
+expect_equal(iofit$iter, glmfit$iter+1)
+expect_equal(iofit$converged, glmfit$converged)
+expect_equal(iofit$formula, glmfit$formula)
+expect_equal(iofit$terms, glmfit$terms)
+expect_equal(iofit$contrasts, glmfit$contrasts)
+
+
