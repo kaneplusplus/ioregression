5852faa1655d97f32f4c027062a63b7599debbbe
taylor <taylor@research.att.com>
2015-02-28 21:16:13 +0000

    Abstract dataframes; major re-write of iolm

---
+++ R/adf.r
@@ -0,0 +1,309 @@
+#' Create an abstract data frame
+#'
+#' @param description    character string.  A description of the connection; the
+#                          path to a file for file connections.
+#' @param conMethod      string indicating the connection method.
+#' @param sep            character seperating the data columns. Ignored when
+#'                         chunkFormatter is given.
+#' @param nsep           character seperating the column of rownames. Set to
+#'                         NA to generate automatic rownames. Ignored when
+#'                         chunkFormatter is given.
+#' @param strict         logical. Whether the parser should run in strict mode.
+#'                         Ignored when chunkFormatter is given.
+#' @param header         logical indicating whether the first line of data, after
+#'                         skip if provided, contains variable names. Ignored if
+#'                         chunkFormatter is also provided, as adf would not know how
+#'                         to parse this first row.
+#' @param colNames       an optional character vector of column names. If missing
+#'                         and header=TRUE, these will be determined be the first
+#'                         row of data. Otherwise, names will be constructed by
+#'                         pasting the character 'V' with the column number.
+#' @param colClasses     an optional character vector of column classes. If
+#'                         named and colNames is missing, the names will be
+#'                         used for colNames. If missing, will be automatically
+#'                         determined.
+#' @param levels         a named list, with names coorisponding to the colNames
+#'                         of class character (or factor). Each element gives the
+#'                         levels for the coorisponding variable. These will be
+#'                         automatically determined when missing.
+#' @param skip           number of lines to strip off of the connection before
+#'                         parsing
+#' @param nrowsClasses   number of rows to pull for determining colClasses and
+#'                         factor levels when needed
+#' @param chunkProcessor a function to apply post-processing to a formatted
+#'                         dataframe. Usually only used without a chunkFormatter.
+#' @param chunkFormatter an optional function turning the raw connection into
+#'                         a dataframe. It must accept four parameters: data,
+#'                         colNames, colClasses, levels. If missing this will
+#'                         be constructed automatically.
+#'
+#' @return An abstract data frame object, which can be used with iolm, ioglm, and
+#'         other related functions.
+#' @export
+adf = function(description, conMethod = c("file", "gzfile", "bzfile", "xzfile"),
+                sep="|", nsep=NA, strict=TRUE, header=FALSE,
+                colNames, colClasses, levels = list(),
+                skip=0L, nrowsClasses=100L, chunkProcessor = identity, chunkFormatter) {
+
+  if (missing(colNames) && !missing(colClasses) && !is.null(names(colClasses)))
+    colNames = names(colClasses)
+  if (header && !missing(chunkFormatter)) {
+    warning("Header not used with user-defined chunkFormatter")
+    header = FALSE
+  }
+  output = list(chunkProcessor = chunkProcessor, skip = skip + header, levels=list())
+
+  # Construct the connection method
+  if (is.function(conMethod)) {
+    output$createNewConnection = conMethod
+  } else {
+    description = path.expand(description)
+    conMethod = match.arg(conMethod)
+    output$createNewConnection = call(conMethod, description=description, open="rb")
+  }
+
+  # Build the chunkFormatter
+  needHead = (missing(colNames) || missing(colClasses) ||
+              length(setdiff(colNames[colClasses %in% c("character", "factor")],names(levels))))
+  if (needHead) {
+    on.exit(close(con))
+    if (is.function(output$createNewConnection)) {
+      con = output$createNewConnection()
+    } else {
+      con = eval(output$createNewConnection)
+    }
+    readLines(con, n=output$skip - header)
+    if (missing(colNames) && header) {}
+      output$colNames = strsplit(readLines(con,1L), split=sep, fixed=TRUE)[[1]]
+    if (missing(chunkFormatter)) {
+      z = as.data.frame(mstrsplit(readLines(con, n=nrowsClasses), sep=sep, nsep=nsep),
+                        stringsAsFactors=FALSE)
+      if (missing(colNames) && !header)
+        output$colNames = sprintf("V%d", seq(ncol(z)))
+    } else {
+      z = chunkFormatter(readLines(con, n=nrowsClasses),
+                          ifelse(missing(colNames), NULL, colNames),
+                          ifelse(missing(colClasses), NULL, colClasses),
+                          output$levels)
+      output$colNames = names(z)
+    }
+    if (missing(colClasses))
+      output$colClasses = sapply(z,function(v) class(type.convert(v, as.is=TRUE)))
+    output$colClasses[output$colClasses == "factor"] = "character"
+    for (j in which(output$colClasses == "character")) {
+      if (is.null(output$levels[[output$colNames[j]]]))
+        output$levels[[output$colNames[j]]] = levels(factor(z[,j]))
+    }
+  }
+
+  if (length(output$colNames) != length(output$colClasses))
+    stop(sprintf("Number of column names (%d) not equal to number of column classes (%d)",
+         length(output$colNames), length(output$colClasses)))
+
+  if (!missing(chunkFormatter)) {
+    output$chunkFormatter = chunkFormatter
+  } else {
+    output$chunkFormatter = .default.chunkFormatter(sep=sep, nsep=nsep, strict=strict)
+  }
+
+  class(output) = c("adf")
+  return(output)
+}
+
+#' Construct a default chunkFormatter
+#'
+#' @param sep     character seperating the data columns.
+#' @param nsep    character seperating the column of rownames. Set to
+#'                  NA to generate automatic rownames.
+#' @param strict  logical. Whether the parser should run in strict mode.
+.default.chunkFormatter = function(sep=sep, nsep=nsep, strict=strict) {
+  function(data, colNames, colClasses, levels) {
+    col_types = colClasses
+    names(col_types) = colNames
+    z = dstrsplit(data, col_types=col_types, sep=sep, nsep=nsep, strict=strict)
+    for (j in which(colClasses == "character")) {
+      if (!is.null(levels[[colNames[j]]]))
+        z[,j] = factor(z[,j], levels=levels[[colNames[j]]])
+    }
+    return(z)
+  }
+}
+
+#' Update abstract data frame with all factor levels
+#'
+#' @param x   an abstract dataframe
+#'
+#' @return a new abstract data frame, with (possibly) new
+#'         factor levels derived from scanning the entire
+#'         dataset.
+#' @export
+allFactorLevels = function(x) {
+  if (!inherits(x, "adf")) stop("x must be an 'adf' object!")
+
+  index = which(x$colClasses == "character")
+  if (length(index) == 0L) return(x)
+
+  x$levels = vector(mode="list", length=length(x$colClasses))
+  names(x$levels) = x$colNames
+  z = adf.apply(x, FUN = function(d) {
+      lapply(d[,index],unique)
+    })
+  for (j in index) {
+    x$levels[[x$colNames[j]]] =
+      sort(unique(unlist(Map(function(w) w[x$colNames[j]], z),
+                                    use.names=FALSE)))
+  }
+  return(x)
+}
+
+#' Convert a data.frame to an abstract data frame
+#'
+#' @param x    a data.frame
+#' @param ...  other arguments. Currently unused
+#'
+#' @return an abstract data frame object
+#' @export
+as.adf = function(x, ...) {
+  output = list()
+
+  output$createNewConnection =
+    charToRaw(paste(iotools::as.output.data.frame(x),collapse="\n"))
+  output$skip = 0L
+  output$chunkProcessor = identity
+  output$chunkFormatter = .default.chunkFormatter("|", NA, TRUE)
+  output$colNames = names(x)
+  output$colClasses = sapply(x, class)
+  output$levels = lapply(x, levels)
+  for (j in which(output$colClasses == "character"))
+    output$levels[[j]] = unique(x[,j])
+
+  class(output) = c("adf")
+  return(output)
+}
+
+#' Low level function for applying over an abstract data frame
+#'
+#' @param x                an abstract data.frame
+#' @param FUN              function to apply over each chunk
+#' @param type             type of data to give as an input to FUN. If model
+#'                         or sparse model, this is a list giving the response (y),
+#'                         model matrix (x), weights (w), and offset (offset)
+#'                         from the input forumal.
+#' @param formula          a formula to use with type equal to model or sparse.model
+#' @param contrasts        contrasts to use with type equal to model or sparse.model
+#' @param subset           a string to to use with type equal to model or sparse.model.
+#'                         Will be evaluated in the environment of the data frame
+#'                         (ex. subset="V2 + V3 > V4")
+#' @param weights          a string to to use with type equal to model or sparse.model.
+#'                         Will be evaluated in the environment of the data frame.
+#' @param na.action        a function which indicates what should happen when the data
+#'                         contain 'NA's. See lm.fit for more details.
+#' @param offset           a string to to use with type equal to model or sparse.model.
+#'                         Will be evaluated in the environment of the data frame.
+#' @param ...              Other arguments to pass.
+#' @param chunk.max.line   integer. Maximum number of lines to parse in a single chunk.
+#' @param CH.MAX.SIZE      integer. Maximum size in bytes of a chunk.
+#' @param CH.MERGE         method for combining the output of chunks. Typically list or rbind.
+#' @param parallel         integer. the number of parallel processes to use in the calculation
+#'                         (*nix only).
+#'
+#' @return an object of class type
+#' @export
+adf.apply = function(x, FUN, type=c("data.frame", "model", "sparse.model"),
+                      formula, contrasts=NULL, subset=NULL, weights=NULL,
+                      na.action=NULL, offset=NULL, ..., chunk.max.line=65536L,
+                      CH.MAX.SIZE=33554432L, CH.MERGE = list, parallel=1L) {
+  if (!inherits(x, "adf")) stop("x must be an 'adf' object!")
+
+  on.exit(close(con))
+  if (is.function(x$createNewConnection)) {
+    con = x$createNewConnection()
+  } else if (is.raw(x$createNewConnection)) {
+    con = rawConnection(x$createNewConnection)
+  } else {
+    con = eval(x$createNewConnection)
+  }
+  if (x$skip > 0L) readLines(con, n=x$skip)
+
+  cr = chunk.reader(con, max.line=chunk.max.line)
+  type = match.arg(type)
+  switch(type,
+    data.frame={
+      FUN2 = function(z) FUN(x$chunkProcessor(x$chunkFormatter(z, x$colNames, x$colClasses, x$levels)))
+    },
+    model={
+      FUN2 = function(z) {
+        df = x$chunkProcessor(x$chunkFormatter(z, x$colNames, x$colClasses, x$levels))
+        mf = match.call(expand.dots = FALSE)[1L]
+        mf$formula = formula
+        mf$data = df
+        if (!is.null(subset)) mf$subset = eval(parse(text=paste0("with(df, ", subset ,")")))
+        if (!is.null(weights)) mf$weights = eval(parse(text=paste0("with(df, ", weights ,")")))
+        if (!is.null(na.action)) mf$na.action = na.action
+        if (!is.null(offset)) mf$offset = eval(parse(text=paste0("with(df, ", offset ,")")))
+        mf$method = "model.frame"
+        mf[[1L]] <- quote(lm.model.frame)
+        mf = eval(mf, parent.frame())
+        mt = attr(mf, "terms")
+        return(FUN(list(y=model.response(mf, "numeric"),
+                        x=model.matrix(mt, mf, contrasts.arg=contrasts),
+                        w=as.vector(model.weights(mf)),
+                        offset=as.vector(model.offset(mf)))))
+      }
+    },
+    sparse.model={
+      FUN2 = function(z) {
+        df = x$chunkProcessor(x$chunkFormatter(z, x$colNames, x$colClasses, x$levels))
+        mf = match.call(expand.dots = FALSE)[1L]
+        mf$formula = formula
+        mf$data = df
+        if (!is.null(subset)) mf$subset = eval(parse(text=paste0("with(df, ", subset ,")")))
+        if (!is.null(weights)) mf$weights = eval(parse(text=paste0("with(df, ", weights ,")")))
+        if (!is.null(na.action)) mf$na.action = na.action
+        if (!is.null(offset)) mf$offset = eval(parse(text=paste0("with(df, ", offset ,")")))
+        mf$method = "model.frame"
+        mf[[1L]] <- quote(lm.model.frame)
+        mf = eval(mf, parent.frame())
+        mt = attr(mf, "terms")
+        return(FUN(list(y=model.response(mf, "numeric"),
+                        x=Matrix::sparse.model.matrix(mt, mf, contrasts.arg=contrasts),
+                        w=as.vector(model.weights(mf)),
+                        offset=as.vector(model.offset(mf)))))
+      }
+    })
+  output = chunk.apply(cr, FUN2, CH.MERGE = CH.MERGE, CH.MAX.SIZE = CH.MAX.SIZE, parallel=parallel)
+  return(output)
+}
+
+#' A custom version of lm to construct a model frame that
+#' does not drop unused levels (because these should be)
+#' in other chunks.
+lm.model.frame =
+function (formula, data, subset, weights, na.action, method = "qr",
+    model = TRUE, x = FALSE, y = FALSE, qr = TRUE, singular.ok = TRUE,
+    contrasts = NULL, offset, ...)
+{
+  ret.x <- x
+  ret.y <- y
+  cl <- match.call()
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data", "subset", "weights", "na.action",
+      "offset"), names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- FALSE
+  mf[[1L]] <- quote(stats::model.frame)
+  mf <- eval(mf, parent.frame())
+  return(mf)
+}
+
+#' Print the details of an abstract data frame
+#'
+#' @param x    a data.frame
+#' @param ...  other arguments. Currently unused
+#'
+#' @export
+print.adf = function(x, ...) {
+  w = min(max(nchar(x$colNames), 10L), 40L)
+  cat(sprintf("    An abstract data frame with %d columns:\n\n", length(x$colClasses)))
+  cat(sprintf(paste0("    %-", w, "s  %-10s"), x$colNames, x$colClasses),sep="\n")
+}
--- R/ols.r
+++ R/ols.r
@@ -1,70 +1,53 @@
-
-# @param formula the formula for the regression
-# @param df the data frame to calculate xtx and xty for
-# @param contrasts the contrasts for categorical regressors
-xtx_and_xty = function(formula, df, contrasts) {
-  mm = model.matrix(formula, df, contrasts=contrasts)
-  xtx = crossprod(mm)
-  xty = crossprod(mm,
-                  matrix(df[row.names(mm), as.character(formula)[2]],
-                          ncol=1))
-  sum_y = sum(df[row.names(mm),as.character(formula)[2]])
-  n = nrow(mm)
-  list(xtx=xtx, xty=xty, sum_y=sum_y, n=n, contrasts=attr(mm, "contrasts"))
-}
-
-# The chunk-wise operation for getting the RSS
-yty_and_btxtxb= function(formula, b, df, contrasts, y_mean) {
-  mm = model.matrix(formula, df, contrasts=contrasts)
-  b_mat = matrix(b[colnames(mm)], nrow=ncol(mm), ncol=1)
-  btxtxb = crossprod(mm %*% b_mat)
-  yty = crossprod(matrix(df[row.names(mm), as.character(formula)[2]], ncol=1))
-  syy = sum((df[row.names(mm), as.character(formula)[2]] - y_mean)^2)
-  rss = sum((df[row.names(mm), as.character(formula)[2]] - mm %*% b_mat)^2)
-  list(yty=yty, btxtxb=btxtxb, syy=syy, rss=rss)
-}
-
 #' Perform a linear regression
 #'
-#' @param form the formula for the regression
-#' @param data a connection to read data from
-#' @param dfpp a function that turns the data read from
-#' a connection into a properly formatted data frame
-#' @param contrasts the contrasts for categorical regressors
-#' @param sep if using a connection or file, which character is used as a separator between elements?
-#' @param parallel how many logical processor cores to use (default 1)
+#' @param formula   the formula for the regression
+#' @param data      an abstract data frame, or something which can be
+#'                  coerced to one.
+#' @param subset    an options character string, which will be evaluated in the
+#'                  frame of the data, to indicate which rows to include
+#'                  in the analysis
+#' @param weights   a optional character string, which will be evaluated in the
+#'                  frame of the data, giving the sample weights for the regression
+#' @param na.action a function which indicates what should happen when the data
+#'                  contain 'NA's. See lm.fit for more details.
+#' @param offset    a optional character string, which will be evaluated in the
+#'                  frame of the data, giving the offsets for the regression
+#' @param contrasts contrasts to use with the regression. See the \code{contrasts.arg}
+#'                  of \code{model.matrix.default}
+#' @param tol       numeric tolerance. Set to -1 to ignore.
 #' @export
-iolm = function(form, data, dfpp,
-               contrasts=NULL, tol=-1, sep=",", parallel=1) {
+iolm = function(formula, data, subset=NULL, weights=NULL,
+                na.action=NULL, offset=NULL, contrasts=NULL, tol=-1) {
   call = match.call()
-  if (is.data.frame(data)) {
-    cvs = xtx_and_xty(form, data,contrasts)
-    xtx = cvs$xtx
-    xty = cvs$xty
-    sum_y = cvs$sum_y
-    n = cvs$n
-    contrasts=cvs$contrasts
-  } else {
-    cvs = chunk.apply(data,
-      function(x) {
-        df = dfpp(x)
-        xtx_and_xty(form, df, contrasts)
-      },
-      CH.MERGE=list, parallel=parallel)
-    xtx = Reduce(`+`, Map(function(x) x$xtx, cvs))
-    xty = Reduce(`+`, Map(function(x) x$xty, cvs))
-    sum_y = Reduce(`+`, Map(function(x) x$sum_y, cvs))
-    n = Reduce(`+`, Map(function(x) x$n, cvs))
-    contrasts = cvs[[1]]$contrasts
-  }
+  if (!inherits(data, "adf")) data = as.adf(data)
+
+  cvs = adf.apply(x=data, type="sparse.model",
+          FUN=function(d) {
+            if (nrow(d$x) == 0L) return(NULL)
+            return(list(xtx = Matrix::crossprod(d$x), xty = Matrix::crossprod(d$x, d$y),
+                        xto = if (!is.null(offset)) Matrix::crossprod(d$x, d$y-d$offset) else NULL,
+                        yty = Matrix::crossprod(d$y), n = nrow(d$x), sum_y = sum(d$y)))
+          },formula=formula,subset=subset,weights=weights,
+            na.action=na.action, offset=offset, contrasts=contrasts)
+  cvs = cvs[!sapply(cvs,is.null)]
+  if (length(cvs) == 0L) stop("No valid data.")
+  xtx = Reduce(`+`, Map(function(x) x$xtx, cvs))
+  xty = Reduce(`+`, Map(function(x) x$xty, cvs))
+  xto = if (!is.null(offset)) Reduce(`+`, Map(function(x) x$xto, cvs)) else 0.0
+  sum_y = Reduce(`+`, Map(function(x) x$sum_y, cvs))
+  yty = Reduce(`+`, Map(function(x) x$yty, cvs))
+  n = Reduce(`+`, Map(function(x) x$n, cvs))
+  contrasts = cvs[[1]]$contrasts
+
   design_matrix_names = colnames(xtx)
+
   # Get rid of colinear variables.
   ch = chol(xtx, pivot=TRUE, tol=tol)
   if (attr(ch, "rank") < ncol(xtx)) {
-    # xtx is rank deficient. 
+    # xtx is rank deficient.
     new_name_order = attr(ch, "pivot")
     effective_rank = attr(ch, "rank")
-    ft = terms(form)
+    ft = terms(formula)
     formula_vars = colnames(attr(ft, "factors"))
     keep_vars = new_name_order[1:effective_rank]
     drop_var_names = colnames(xtx)[setdiff(new_name_order, keep_vars)]
@@ -81,7 +64,7 @@ iolm = function(form, data, dfpp,
       warning(paste("Removing the following variables due to colinearity:",
               colnames(xtx)[tail(new_name_order,nrow(xtx)-effective_rank)],
               sep="\n"))
-    } 
+    }
     #Reorder xtx and xty in terms of the pivots up to the rank.
     xtx = xtx[keep_vars, keep_vars]
     xty = xty[keep_vars,]
@@ -90,91 +73,151 @@ iolm = function(form, data, dfpp,
     ft = drop.terms(ft, drop_inds, keep.response=TRUE)
     form = formula(ft)
   }
-  coefficients = as.numeric(solve(xtx) %*% xty)
+  coefficients = as.numeric(Matrix::solve(xtx,xty-xto)) #as.numeric(solve(xtx) %*% xty)
   names(coefficients) = rownames(xtx)
-  terms = terms(form)
+  terms = terms(formula)
   contrasts = contrasts
-  # If data was a connection then we set data to NULL to since it is not
-  # guaranteed to be "rewindable".
-  if (!is.data.frame(data) && 
-      (inherits(data, "connection") || inherits(data, "ChunkReader"))) {
-    data=NULL
-  }
-  if (missing(dfpp)) dfpp=NULL
-  ret = list(coefficients=coefficients, call=call, terms=terms, 
-             design_matrix_names=design_matrix_names, xtx=xtx, sum_y=sum_y,
-             n=n, data=data, dfpp=dfpp, contrasts=contrasts, rank=ncol(xtx),
-             parallel=parallel)
+
+  ret = list(coefficients=coefficients, call=call, terms=terms,
+             xtx=xtx, xty=xty, xto=xto, yty=yty,
+             sum_y=as.numeric(sum_y), n=n, data=data,
+             contrasts=contrasts, rank=ncol(xtx))
   class(ret) = "iolm"
   ret
 }
 
 #' Get the regression diagnostics for a linear regression
-#' 
+#'
 #' @param object an object return from iolm
-#' @param parallel how many logical processor cores to use (default 1)
 #' @export
-summary.iolm = function(object, parallel, data, ...) {
+summary.iolm = function(object, ...) {
   call = match.call()
-  terms = object$terms
-  if (missing(data)) {
-    data = object$data
-  }
-  if (missing(parallel)) {
-    parallel = object$parallel
-  }
-  if (is.data.frame(data)) {
-    rss_chunk = yty_and_btxtxb(formula(object$terms), 
-                               object$coefficients,
-                               data,
-                               object$coefficients,
-                               object$sum_y/object$n)
-    yty = rss_chunk$yty
-    btxtxb = rss_chunk$btxtxb
-    syy = rss_chunk$syy
-    rss = rss_chunk$rss
-  } else {
-    #input = if (is.character(data)) input.file(data) else data
-    rss_chunks = chunk.apply(data,
-      function(x) {
-        df = object$dfpp(x)
-        yty_and_btxtxb(formula(object$terms),
-                       object$coefficients, 
-                       df, 
-                       object$contrasts,
-                       object$sum_y/object$n)
-      },
-      CH.MERGE=list, parallel=parallel)
-    yty = Reduce(`+`, Map(function(x) x$yty, rss_chunks))
-    btxtxb = Reduce(`+`, Map(function(x) x$btxtxb, rss_chunks))
-    syy = Reduce(`+`, Map(function(x) x$syy, rss_chunks))
-    rss = Reduce(`+`, Map(function(x) x$rss, rss_chunks))
-  }
-  rss_beta = yty - btxtxb
-  df = c(length(object$design_matrix_names), 
-         object$n-length(object$design_matrix_names),
-         length(object$design_matrix_names))
+
+  beta = coef(object)
+  btxtxb = t(beta) %*% object$xtx %*% beta
+  rss = object$yty + btxtxb - 2 * t(beta) %*% object$xty
+  tss = object$yty - object$sum_y^2 / object$n
+  mss <- if (attr(object$terms, "intercept"))
+            (tss - rss) else as.numeric(btxtxb)
+
+  rss_beta = object$yty - btxtxb
+  df = c(length(beta),
+         object$n-length(beta),
+         length(beta))
   sigma = as.numeric(sqrt(rss_beta / df[2]))
-  #vcv = sigma * sqrt(solve(object$xtx)) 
+  #vcv = sigma * sqrt(solve(object$xtx))
   cov_unscaled = solve(object$xtx)
   se = sigma * sqrt(diag(cov_unscaled))
   tv = object$coefficients/se
-  ptv = apply(cbind( pt(tv, df[2]), 1-pt(tv, df[2]) ), 1, min)
+  ptv = 2 * pt(abs(tv), df[2], lower.tail = FALSE)
+
   coefficients = cbind(object$coefficients, se, tv, ptv)
   colnames(coefficients) = c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
   rownames(coefficients) = rownames(object$xtx)
+
   # Aliasing could be better.
   aliased = rep(FALSE, length(object$coefficients))
   names(aliased) = names(object$coefficients)
-  r_squared = 1 - rss/syy
-  adj_r_squared = 1 - (1-r_squared) * sum(df[1:2], -1)/df[2]
-  fstatistic = c((syy - rss)/sigma^2/(df[1]-1), df[1]-1, dendf=df[2])
+  df.int <- if (attr(object$terms, "intercept")) 1L else 0L
+  r_squared = as.numeric(mss/(mss + rss))
+  adj_r_squared = 1 - (1-r_squared) * sum(df[1:2], -1*df.int)/df[2]
+  fstatistic = c((mss/(df[1]-df.int))/sigma^2, df[1]-df.int, dendf=df[2])
   names(fstatistic) = c("value", "numdf", "dendf")
+
   ret = list(call=call, terms=object$terms, coefficients=coefficients,
        aliased=aliased, sigma=sigma, df=df, r.squared=r_squared,
-       adj.r.squared=adj_r_squared, fstatistic=fstatistic, 
-       cov.unscaled=cov_unscaled, data=object$data, dfpp=object$dfpp,
-       parallel=parallel)
-  class(ret) = "summary.lm"
+       adj.r.squared=as.numeric(adj_r_squared),
+       fstatistic=sapply(fstatistic,as.numeric),
+       cov.unscaled=cov_unscaled, data=object$data, dfpp=object$dfpp)
+  class(ret) = "summary.iolm"
   ret
+}
+
+#' Print iolm object
+#'
+#' @param x        output of iolm
+#' @param digits   significant digits to print
+#' @export
+print.iolm =
+function (x, digits = max(3L, getOption("digits") - 3L), ...)
+{
+    cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
+        "\n\n", sep = "")
+    if (length(coef(x))) {
+        cat("Coefficients:\n")
+        print.default(format(coef(x), digits = digits), print.gap = 2L,
+            quote = FALSE)
+    }
+    else cat("No coefficients\n")
+    cat("\n")
+    invisible(x)
+}
+
+#' Print iolm summary
+#'
+#' @param x             output of summary.iolm
+#' @param digits        significant digits to print
+#' @param symbolic.cor  logical. Should symbolic correlation be printed.
+#' @param signif.stars  logical. Should significant starts be printed.
+#' @export
+print.summary.iolm =
+function (x, digits = max(3L, getOption("digits") - 3L), symbolic.cor = x$symbolic.cor,
+    signif.stars = getOption("show.signif.stars"), ...)
+{
+  cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
+      "\n\n", sep = "")
+  resid <- x$residuals
+  df <- x$df
+  rdf <- df[2L]
+  if (length(x$aliased) == 0L) {
+      cat("\nNo Coefficients\n")
+  }
+  else {
+    if (nsingular <- df[3L] - df[1L])
+        cat("\nCoefficients: (", nsingular, " not defined because of singularities)\n",
+            sep = "")
+    else cat("\nCoefficients:\n")
+    coefs <- x$coefficients
+    if (!is.null(aliased <- x$aliased) && any(aliased)) {
+        cn <- names(aliased)
+        coefs <- matrix(NA, length(aliased), 4, dimnames = list(cn,
+            colnames(coefs)))
+        coefs[!aliased, ] <- x$coefficients
+    }
+    printCoefmat(coefs, digits = digits, signif.stars = signif.stars,
+        na.print = "NA", ...)
+  }
+  cat("\nResidual standard error:", format(signif(x$sigma,
+      digits)), "on", rdf, "degrees of freedom")
+  cat("\n")
+  if (nzchar(mess <- naprint(x$na.action)))
+      cat("  (", mess, ")\n", sep = "")
+  if (!is.null(x$fstatistic)) {
+    cat("Multiple R-squared: ", formatC(x$r.squared, digits = digits))
+    cat(",\tAdjusted R-squared: ", formatC(x$adj.r.squared,
+        digits = digits), "\nF-statistic:", formatC(x$fstatistic[1L],
+        digits = digits), "on", x$fstatistic[2L], "and",
+        x$fstatistic[3L], "DF,  p-value:", format.pval(pf(x$fstatistic[1L],
+            x$fstatistic[2L], x$fstatistic[3L], lower.tail = FALSE),
+            digits = digits))
+    cat("\n")
+  }
+  correl <- x$correlation
+  if (!is.null(correl)) {
+    p <- NCOL(correl)
+    if (p > 1L) {
+      cat("\nCorrelation of Coefficients:\n")
+      if (is.logical(symbolic.cor) && symbolic.cor) {
+        print(symnum(correl, abbr.colnames = NULL))
+      }
+      else {
+        correl <- format(round(correl, 2), nsmall = 2,
+          digits = digits)
+        correl[!lower.tri(correl)] <- ""
+        print(correl[-1, -p, drop = FALSE], quote = FALSE)
+      }
+    }
+  }
+  cat("\n")
+  invisible(x)
 }
