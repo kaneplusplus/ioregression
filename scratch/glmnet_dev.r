b187ccc75b22c1abdb4fc97c8b401c7d2998e4b1
Michael Kane <kaneplusplus@gmail.com>
2015-04-27 12:35:35 -0400

    STRONG and SAFE coefficient filtering has been implemented.

--- scratch/glmnet_dev.r
+++ scratch/glmnet_dev.r
@@ -30,10 +30,11 @@ covariance_update = function(y, x, beta, lambda, alpha, tol=0.0001) {
 
 beta = Matrix(1, nrow=ncol(x))
 beta_old = -beta
-lambda = 0.5
+lambda = .1
 alpha = 0.5
 tol = 0.01
 
+# elasticnet using no filtering of beta coefficients
 while( as.vector(crossprod(beta-beta_old)) > tol ) {
   beta_old = beta
   beta = covariance_update(y, x, beta, lambda, alpha)
@@ -43,7 +44,43 @@ while( as.vector(crossprod(beta-beta_old)) > tol ) {
 beta
 
 # Is to:
-glmnet(x, y, lambda = seq(1, 0, by=-0.05), alpha=0.5)$beta[,"s10"]
+glmnet(x, y, lambda = lambda, alpha=alpha)$beta
 
 
+# Filter coefficients using the SAFE rules. Note this assumes 
+# columns of x have been standardized.
+# The functions returns whether or not you should keep beta at
+# the corresponding positions (TRUE means keep it).
+filter_safe = function(y, x, lambda, lambda_max=lambda) {
+  xty = crossprod(x, y)
+  xty >= lambda - sd(y) * (lambda_max - lambda) / lambda_max 
+}
+
+filter_strong = function(y, x, lambda, lambda_max=lambda) {
+  crossprod(x, y) >= 2*lambda - lambda_max
+}
+
+# elasticnet using SAFE filtering of beta coefficients
+beta = Matrix(as.numeric(filter_safe(y, x, lambda), nrow=ncol(x)))
+beta_old = -beta
+while( as.vector(crossprod(beta-beta_old)) > tol ) {
+  beta_old = beta
+  beta = covariance_update(y, x, beta, lambda, alpha)
+}
+
+# SAFE
+beta
+
+beta = Matrix(as.numeric(filter_strong(y, x, lambda), nrow=ncol(x)))
+beta_old = -beta
+while( as.vector(crossprod(beta-beta_old)) > tol ) {
+  beta_old = beta
+  beta = covariance_update(y, x, beta, lambda, alpha)
+}
+
+# STRONG
+beta
+
+# Is to:
+glmnet(x, y, lambda = lambda, alpha=alpha)$beta
 
