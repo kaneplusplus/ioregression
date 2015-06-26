e949a6bf51d041709f676628f020f51000e4ffa6
Michael Kane <kaneplusplus@gmail.com>
2015-06-26 17:36:49 -0400

    Intercepts are now handled correctly.

--- R/lmnet.r
+++ R/lmnet.r
@@ -51,13 +51,12 @@ soft_thresh = function(x, g) {
 #'                     calculation (*nix only).
 #' @export
 iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
-                 offset=NULL, alpha=1, lambda=NULL, contrasts=NULL,
-                 standardize=TRUE, tol=1e-7,
-                 max_it = 1e+05, filter=c("strong", "safe", "none"),
-                 lambda_epsilon=0.0001, nlambdas=100,parallel=1L) {
+                   offset=NULL, alpha=1, lambda=NULL, contrasts=NULL,
+                   standardize=FALSE, tol=1e-7,
+                   max_it = 1e+05, filter=c("strong", "safe", "none"),
+                   lambda_epsilon=0.0001, nlambdas=100,parallel=1L) {
 
   # Under-development related messages.
-  if (!standardize) stop("Unstandardizing data is not yet supported.")
   if (!missing(weights)) stop("Weights are not yet supported.")
 
   call = match.call()
@@ -70,14 +69,18 @@ iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
   if (!is.null(offset) && !is.character(offset <- offset[[1]]))
     stop("offset must be a length one character vector")
 
-  # Get the standardization information as well as the
-  # lambdas from the data. Note that this will take two passes. One to get
-  # the means, one to get the variances. If you try to do it in a single
-  # pass you start to lose stability.
+  # Here's where we get the initial xtx, xty and number of rows. Note that
+  # if we standardize that we need an extra pass through the data; one
+  # to get the mans, one to get the standardized inner-products. If you try
+  # to do it in a single pass you will lose stability.
   if (standardize) {
     stand_info = adf.apply(x=data, type="sparse.model",
       FUN=function(d,passedVars) {
         if (nrow(d$x) == 0L) return(NULL)
+        if (colnames(d$x)[1] == "(Intercept)") {
+          # Get rid of the intercept column.
+          d$x = d$x[,2:ncol(d$x),drop=FALSE]
+        }
         if (!is.null(d$offset)) d$y = d$y - d$offset
         if (!is.null(d$w)) {
           if (any(d$w == 0)) {
@@ -100,39 +103,35 @@ iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
                     col_sum_x_squared = Matrix::colSums(d$x^2),
                     sum_y = sum(d$y),
                     contrasts=attr(d$x, "contrasts"),
-                    all_var_names = colnames(d$x),
-                    xty_unstandardized=Matrix::crossprod(d$x, d$y)))
+                    all_var_names = colnames(d$x), 
+                    warn_intercept=attributes(d$mt)$intercept))
 
       },formula=formula,subset=subset,weights=weights,
         na.action=na.action, offset=offset, contrasts=contrasts,
         parallel=parallel)
     stand_info = stand_info[!sapply(stand_info, is.null)]
     if (length(stand_info) == 0L) stop("No valid data.")
-<<<<<<< HEAD
     num_rows = Reduce(`+`, Map(function(x) x$num_rows, stand_info))    
     sum_w = Reduce(`+`, Map(function(x) x$sum_w , stand_info))    
     col_sum_x = Reduce(`+`, Map(function(x) x$col_sum_x, stand_info))
     col_mean_x = col_sum_x / num_rows
     sum_y = Reduce(`+`, Map(function(x) x$sum_y, stand_info)) 
-=======
-    num_rows = Reduce(`+`, Map(function(x) x$num_rows, stand_info))
-    sum_w = Reduce(`+`, Map(function(x) x$sum_w , stand_info))
-    col_mean_x = Reduce(`+`,
-      Map(function(x) x$col_sum_x, stand_info)) / num_rows
-    sum_y = Reduce(`+`, Map(function(x) x$sum_y, stand_info))
->>>>>>> 003cadaccb446863d1e1d52b02d5b79a8da078a1
     mean_y = sum_y / num_rows
     col_sum_x_squared = Reduce(`+`,
       Map(function(x) x$col_sum_x_squared, stand_info))
     contrasts=stand_info[[1]]$contrasts
     all_var_names = stand_info[[1]]$all_var_names
-    xty_unstandardized = Reduce(`+`, Map(function(x) x$xty_unstandardized,
-      stand_info))
+    if (stand_info[[1]]$warn_intercept) 
+      warning("Intercept is ignored when variables are standardized")
 
     # Get the standard deviations and then fix the lambdas.
     stand_info = adf.apply(x=data, type="sparse.model",
       FUN=function(d,passedVars) {
         if (nrow(d$x) == 0L) return(NULL)
+        if (colnames(d$x)[1] == "(Intercept)") {
+          # Get rid of the intercept column.
+          d$x = d$x[,2:ncol(d$x),drop=FALSE]
+        }
         if (!is.null(d$offset)) d$y = d$y - d$offset
         if (!is.null(d$w)) {
           if (any(d$w == 0)) {
@@ -149,13 +148,14 @@ iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
         } else {
           sum_w = nrow(d$x)
         }
-        x_bar = Matrix::Matrix(col_mean_x,ncol=ncol(d$x), nrow=nrow(d$x), byrow=TRUE)
+        x_bar = Matrix::Matrix(col_mean_x,ncol=ncol(d$x), nrow=nrow(d$x), 
+                               byrow=TRUE)
         x_centered=d$x-x_bar
         y_centered=d$y-mean_y
-        return(list(x=d$x,
-          col_x_square_diff= Matrix::colSums(x_centered^2),
-          y_square_diff= sum(y_centered^2),
-          centered_xty= Matrix::crossprod(x_centered, y_centered)))
+        return(list(col_x_square_diff=Matrix::colSums(x_centered^2),
+                    y_square_diff=sum(y_centered^2),
+                    centered_xty=Matrix::crossprod(x_centered, y_centered),
+                    all_var_names=colnames(d$x)))
 
       },formula=formula,subset=subset,weights=weights,
         na.action=na.action, offset=offset, contrasts=contrasts)
@@ -164,16 +164,77 @@ iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
       x_sd = sqrt(col_x_square_diff / (num_rows-1))
       y_square_diff = Reduce(`+`, Map(function(x) x$y_square_diff, stand_info))
       y_sd = sqrt(y_square_diff / (num_rows-1))
-      normalized_xty = Reduce(`+`,
+      xty = Reduce(`+`,
         Map(function(x) x$centered_xty, stand_info)) / x_sd / y_sd
+      all_var_names = stand_info[[1]]$all_var_names
+
+    # Second pass in the standardized case.
+    cov_update_info = adf.apply(x=data, type="sparse.model",
+      FUN=function(d,passedVars) {
+        #d$x = d$x[,active_regressors, drop=FALSE]
+        if (nrow(d$x) == 0L) return(NULL)
+        if (colnames(d$x)[1] == "(Intercept)") {
+          # Get rid of the intercept column.
+          d$x = d$x[,2:ncol(d$x),drop=FALSE]
+        }
+        if (!is.null(d$offset)) d$y = d$y - d$offset
+        if (!is.null(d$w)) {
+          if (any(d$w == 0)) {
+            ok = d$w != 0
+            d$w = d$w[ok]
+            d$x = d$x[ok,,drop = FALSE]
+            d$y = d$y[ok]
+            if (!is.null(d$offset)) d$offset = d$offset[ok]
+          }
+          d$x = d$x * sqrt(d$w)
+          d$y = d$y * sqrt(d$w)
+        } else {
+          sum_w = nrow(d$x)
+        }
+        d$x=(d$x-Matrix::Matrix(col_mean_x,
+             ncol=ncol(d$x), nrow=nrow(d$x), byrow=TRUE)) /
+             Matrix::Matrix(x_sd, ncol=ncol(d$x), nrow=nrow(d$x),
+                    byrow=TRUE)
+        d$y = (d$y - mean_y) / y_sd
+        return(list(xtx = Matrix::crossprod(d$x)))
+      },formula=formula,subset=subset,weights=weights,
+        na.action=na.action, offset=offset, contrasts=contrasts)
+    cov_update_info = cov_update_info[!sapply(cov_update_info, is.null)]
+    xtx_all = Reduce(`+`, Map(function(x) x$xtx, cov_update_info))
   } else {
-    stop("Non-standardized regressors are not yet supported.")
+
+    # Unstandardized.
+    stand_info = adf.apply(x=data, type="sparse.model",
+      FUN=function(d,passedVars) {
+        if (nrow(d$x) == 0L) return(NULL)
+        if (!is.null(d$offset)) d$y = d$y - d$offset
+        if (!is.null(d$w)) {
+          if (any(d$w == 0)) {
+            ok = d$w != 0
+            d$w = d$w[ok]
+            d$x = d$x[ok,,drop = FALSE]
+            d$y = d$y[ok]
+            if (!is.null(d$offset)) d$offset = d$offset[ok]
+          }
+          d$x = d$x * sqrt(d$w)
+          d$y = d$y * sqrt(d$w)
+        } else {
+          sum_w = nrow(d$x)
+        }
+        return(list(num_rows=nrow(d$x), xty=Matrix::crossprod(d$x, d$y),
+                    xtx=Matrix::crossprod(d$x),
+                    contrasts=attr(d$x, "contrasts"),
+                    all_var_names=colnames(d$x)))
+
+      },formula=formula,subset=subset,weights=weights,
+        na.action=na.action, offset=offset, contrasts=contrasts)
+      xty = Reduce(`+`, Map(function(x) x$xty, stand_info))
+      xtx_all = Reduce(`+`, Map(function(x) x$xtx, stand_info))
+      num_rows = Reduce(`+`, Map(function(x) x$num_rows, stand_info))    
+      all_var_names=stand_info[[1]]$all_var_names
   }
 
-
-  data_lambdas = abs(normalized_xty) / num_rows / alpha
-  data_lambdas_unstandardized = xty_unstandardized
-
+  data_lambdas = abs(xty) / num_rows / alpha
   lambda_k = max(abs(data_lambdas))
   if (is.null(lambda)) {
     lambda_path = seq(from=lambda_k, to=lambda_epsilon*lambda_k,
@@ -184,42 +245,8 @@ iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
   beta_path = NULL
   lambda = c()
 
-  cov_update_info = adf.apply(x=data, type="sparse.model",
-    FUN=function(d,passedVars) {
-      #d$x = d$x[,active_regressors, drop=FALSE]
-      if (nrow(d$x) == 0L) return(NULL)
-      if (!is.null(d$offset)) d$y = d$y - d$offset
-      if (!is.null(d$w)) {
-        if (any(d$w == 0)) {
-          ok = d$w != 0
-          d$w = d$w[ok]
-          d$x = d$x[ok,,drop = FALSE]
-          d$y = d$y[ok]
-          if (!is.null(d$offset)) d$offset = d$offset[ok]
-        }
-        d$x = d$x * sqrt(d$w)
-        d$y = d$y * sqrt(d$w)
-      } else {
-        sum_w = nrow(d$x)
-      }
-      if (standardize) {
-        d$x=(d$x-Matrix::Matrix(col_mean_x,
-             ncol=ncol(d$x), nrow=nrow(d$x), byrow=TRUE)) /
-             Matrix::Matrix(x_sd, ncol=ncol(d$x), nrow=nrow(d$x),
-                    byrow=TRUE)
-        d$y = (d$y - mean_y) / y_sd
-      } else {
-        stop("Unstandardized not supported")
-      }
-      return(list(xtx = Matrix::crossprod(d$x)))
-    },formula=formula,subset=subset,weights=weights,
-      na.action=na.action, offset=offset, contrasts=contrasts)
-  cov_update_info = cov_update_info[!sapply(cov_update_info, is.null)]
-  xtx_all = Reduce(`+`, Map(function(x) x$xtx, cov_update_info))
-
   # Note that we'll need to do something with the a0 when we're not
   # standardizing. For now they are 0.
-  a0 = c()
   # The following could be parallelized.
   for(lambda in lambda_path) {
     if (filter[1] == "strong") {
@@ -244,7 +271,7 @@ iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
       while (it_num <= max_it &&
              as.vector(Matrix::crossprod(beta-beta_old)) > tol) {
         beta_old = beta
-        ud = normalized_xty[active_regressors,,drop=FALSE] - xtx %*% beta
+        ud = xty[active_regressors,,drop=FALSE] - xtx %*% beta
         beta = soft_thresh(ud / num_rows + beta, lambda*alpha) /
           (1 + lambda*(1-alpha))
         it_num = it_num + 1
@@ -265,9 +292,9 @@ iolmnet = function(formula, data, subset=NULL, weights=NULL, na.action=NULL,
     } else {
       beta_path = cbind(beta_path, beta_ret)
     }
-    a0 = c(a0, mean_y)
   }
-  lambda_path_unnormalized = lambda*x_sd*y_sd + 
+  # TODO: this needs to be fixed.
+  a0 = rep(0, length(lambda_path)) 
   list(call=call, a0=a0, beta=beta_path, lambda = lambda_path)
 }
 
--- tests/testthat/test-iolmnet.r
+++ tests/testthat/test-iolmnet.r
@@ -9,11 +9,20 @@ names(df) = gsub("V", "X", names(df))
 
 alpha=0.5
 
-iofit = iolmnet(Y ~ .-1, df, alpha=alpha)
-
-#col_means = colMeans(x)
-#x = x - matrix(data=col_means, ncol=ncol(x), nrow=nrow(x), byrow=TRUE)
-#col_sd = apply(x, 2, sd)
-#x = x / matrix(data=col_sd, ncol=ncol(x), nrow=nrow(x), byrow=TRUE)
-#y = (y-mean(y)) / sd(y)
+iofit = iolmnet(Y ~ .-1, df, alpha=alpha, standardize=FALSE)
 fit = glmnet(x, y, alpha=alpha)
+
+print(fit$beta[,ncol(fit$beta)])
+print(iofit$beta[,ncol(iofit$beta)])
+
+
+iofit = iolmnet(Y ~ ., df, alpha=alpha, standardize=TRUE)
+col_means = colMeans(x)
+x = x - matrix(data=col_means, ncol=ncol(x), nrow=nrow(x), byrow=TRUE)
+col_sd = apply(x, 2, sd)
+x = x / matrix(data=col_sd, ncol=ncol(x), nrow=nrow(x), byrow=TRUE)
+y = (y-mean(y)) / sd(y)
+fit = glmnet(x, y, alpha=alpha)
+
+print(fit$beta[,ncol(fit$beta)])
+print(iofit$beta[,ncol(iofit$beta)])
