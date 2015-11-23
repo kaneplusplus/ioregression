ece761eb9e8dbbfa0bc5558195134483441fb03b
Michael Kane <kaneplusplus@gmail.com>
2015-11-23 16:56:36 -0500

    Working but there is a problem with stability.

--- inst/ref-impl/glmnet-ref.r
+++ inst/ref-impl/glmnet-ref.r
@@ -12,31 +12,38 @@ soft_thresh = function(x, g) {
 }
 
 lmnet_ref = function(X, y, lambda, alpha, maxit=10000, tol=1e-7) {
-  beta = rep(1, ncol(X))
+  beta = rep(0, ncol(X))
   xty = crossprod(X, y)
   xtx = crossprod(X)
   for(j in 1:maxit) {
     beta_old = beta
-    beta = soft_thresh((xty - xtx %*% beta)/nrow(X) + beta, lambda*alpha)
-    beta = beta / (1 + lambda*(1-alpha))
+    #beta = soft_thresh((xty - xtx %*% beta)/nrow(X) + beta, lambda*alpha)
+    for (l in 1:length(beta)) {
+      beta[l] = soft_thresh(sum(X[,l]*(y - X[,-l] %*% beta_old[-l])),
+                            nrow(X)*lambda*alpha)
+    }
+    beta = beta / (colSums(X^2) + lambda*(1-alpha))
     if(sqrt(crossprod(beta-beta_old)) < tol) break
   }
   list(beta=beta, iterations=j)
 }
 
-x=matrix(rnorm(100*20),100,20)
-y=rnorm(100)
+#set.seed(3)
+x=matrix(rnorm(100*20),100,20) * 100
+y= sample(0:1,100,replace=TRUE)
 fit=glmnet(x,y, lambda=0.0454336178, standardize=FALSE, intercept=FALSE)
 
 fit_ref = lmnet_ref(x, y, lambda=0.0454336178, alpha=1)
 crossprod(fit_ref$beta, fit$beta)
+print("LMNET")
+
+print(cbind(fit$beta, fit_ref$beta))
 
 max(abs(fit_ref$beta - fit$beta))
 
 glmnet_ref = function(X, y, lambda, alpha, family=binomial, maxit=10, tol=1e-08)
 {
   beta = matrix(rep(0,ncol(X)), ncol=1)
-  resids = c()
   for(j in 1:maxit)
   {
     beta_outer_old = beta
@@ -50,7 +57,7 @@ glmnet_ref = function(X, y, lambda, alpha, family=binomial, maxit=10, tol=1e-08)
       beta_inner_old = beta
       for (l in 1:length(beta)) {
         beta[l] = soft_thresh(sum(W*X[,l]*(z - X[,-l] %*% beta_inner_old[-l])), 
-                              nrow(X)*lambda*alpha)
+                              sum(W)*lambda*alpha)
       }
       beta = beta / (wx_norm + lambda*(1-alpha))
       if(sqrt(as.double(crossprod(beta-beta_inner_old))) < tol) break
@@ -60,25 +67,28 @@ glmnet_ref = function(X, y, lambda, alpha, family=binomial, maxit=10, tol=1e-08)
   list(beta=beta,iterations=j)
 }
 
+set.seed(3)
 x = matrix(rnorm(100*20),100,20) * 100 
 #x <- scale(x)
 g2 = sample(0:1,100,replace=TRUE)
 
+
 #lambda = 0
-lambda = 0.0285513653
+lambda = 0.0454336178
 
 fg = glmnet(x, g2, family="gaussian", lambda=lambda, standardize=FALSE, intercept=FALSE)
 fit = glmnet_ref(x, g2, family=gaussian, lambda=lambda, alpha=1, tol=0)
 
 max(abs(fg$beta - fit$beta))
-cbind(fg$beta, fit$beta)
+print("GLMNET GAUSSIAN")
+print(cbind(fg$beta, fit$beta))
 
 fg = glmnet(x, g2, family="binomial", lambda=lambda, standardize=FALSE, intercept=FALSE)
 fit = glmnet_ref(x, g2, family=binomial, lambda=lambda, alpha=1, tol=0)
 
 print(max(abs(fg$beta - fit$beta)))
-cbind(fg$beta, fit$beta)
-stop("here")
+print("GLMNET BINOMIAL")
+print(cbind(fg$beta, fit$beta))
 
 fit$beta
 fit$iterations
