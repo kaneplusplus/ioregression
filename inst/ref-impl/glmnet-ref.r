70358400a61f8c0dbf41147292bfe13597da51ba
Michael Kane <kaneplusplus@gmail.com>
2015-11-23 21:08:07 -0500

    Changed the convergence criteria for inner loop of glmnet.

--- inst/ref-impl/glmnet-ref.r
+++ inst/ref-impl/glmnet-ref.r
@@ -28,7 +28,7 @@ lmnet_ref = function(X, y, lambda, alpha, maxit=10000, tol=1e-7) {
   list(beta=beta, iterations=j)
 }
 
-#set.seed(3)
+set.seed(3)
 x=matrix(rnorm(100*20),100,20) * 100
 y= sample(0:1,100,replace=TRUE)
 fit=glmnet(x,y, lambda=0.0454336178, standardize=FALSE, intercept=FALSE)
@@ -38,12 +38,13 @@ crossprod(fit_ref$beta, fit$beta)
 print("LMNET")
 
 print(cbind(fit$beta, fit_ref$beta))
-
+stop("here")
 max(abs(fit_ref$beta - fit$beta))
 
 glmnet_ref = function(X, y, lambda, alpha, family=binomial, maxit=10, tol=1e-08)
 {
   beta = matrix(rep(0,ncol(X)), ncol=1)
+  ql = c()
   for(j in 1:maxit)
   {
     beta_outer_old = beta
@@ -53,25 +54,31 @@ glmnet_ref = function(X, y, lambda, alpha, family=binomial, maxit=10, tol=1e-08)
     z      = eta + (y - g) / gprime
     W      = as.vector(gprime^2 / family()$variance(g))
     wx_norm = colSums(W*X^2)
+    quad_loss = Inf
     for (k in 1:maxit) {
+      quad_loss_old = Inf
       beta_inner_old = beta
       for (l in 1:length(beta)) {
         beta[l] = soft_thresh(sum(W*X[,l]*(z - X[,-l] %*% beta_inner_old[-l])), 
                               sum(W)*lambda*alpha)
       }
       beta = beta / (wx_norm + lambda*(1-alpha))
-      if(sqrt(as.double(crossprod(beta-beta_inner_old))) < tol) break
+      quad_loss = -1/2/nrow(X) * sum(W*(z - X %*% beta)^2) + 
+        lambda * (1-alpha) * sum(beta^2)/2 + alpha * sum(beta)
+      if (quad_loss > quad_loss_old) quad_loss_old = quad_loss
+      else break
+      #if(sqrt(as.double(crossprod(beta-beta_inner_old))) < tol) break
     }
+    ql = c(ql, quad_loss)
     if (sqrt(as.double(crossprod(beta-beta_outer_old))) < tol) break
   }
-  list(beta=beta,iterations=j)
+  list(beta=beta,iterations=j, ql=ql)
 }
 
 set.seed(3)
 x = matrix(rnorm(100*20),100,20) * 100 
 #x <- scale(x)
 g2 = sample(0:1,100,replace=TRUE)
-
 
 #lambda = 0
 lambda = 0.0454336178
